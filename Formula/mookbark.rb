c99bfd9ac95e3ef2f51a6ee636f0ce61de37d24c
Furkan Kalaycioglu <furkann12@hotmail.com>
2024-02-27 20:27:59 +0300

    Use separated cliAuth dist

--- Formula/mookbark.rb
+++ Formula/mookbark.rb
@@ -2,17 +2,17 @@ class Mookbark < Formula
   desc "A bookmark manager"
   homepage "https://mookbark.run"
   url "https://github.com/furkankly/mookbark-release/releases/download/v0.1.0/mookbark-0.1.0.tar.gz"
-  sha256 "1b18006a45ba79539cb31b6cf96d86b4ef82fa4715064b452e83a7c881d1bc3b"
+  sha256 "37a250f362130c8992a6c44da2fdee3d65ef4d5fb96f6e57c283787bf129dc2b"
   version "0.1.0"
 
   def install
     bin.install "mookbark"
 
-    # Create a directory for dist within the Cellar directory
-    dist_dir = share/"mookbark/dist"
+    # Create a directory for dist-cliAuth within the Cellar directory
+    dist_dir = share/"mookbark/dist-cliAuth"
     dist_dir.mkpath
 
-    # Install files and folders from the "dist" directory within the formula
-    dist_dir.install Dir["dist/*"]
+    # Install files and folders from the dist directory within the formula
+    dist_dir.install Dir["dist-cliAuth/*"]
   end
 end
