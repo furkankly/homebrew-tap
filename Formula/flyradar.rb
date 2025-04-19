e892a795e1b7a34d1e789edd41f7b31d44d5595a
Furkan Kalaycioglu <furkann12@hotmail.com>
2025-04-19 17:51:21 +0300

    flyradar 0.2.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/flyradar.rb
+++ Formula/flyradar.rb
@@ -1,9 +1,9 @@
 class Flyradar < Formula
   desc "flyradar - Manage your Fly.io resources in style"
   homepage "https://github.com/furkankly/flyradar"
-  url "https://github.com/furkankly/flyradar/releases/download/0.2.0/flyradar-0.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "3ca4532a56bf86ea119251621617851fbf10a54f9b0838fc64c6de55bb223041"
-  version "0.2.0"
+  url "https://github.com/furkankly/flyradar/releases/download/0.2.1/flyradar-0.2.1-x86_64-apple-darwin.tar.gz"
+  sha256 "8ec7588772e82057d5d1ac44b85167039a333ba421cfa3baa94860bbad5e3127"
+  version "0.2.1"
   
   def install
     bin.install "flyradar"
